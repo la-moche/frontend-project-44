932698ce687981d98f2466a405b0407fa5d0957b
Alexander Smirnov <priamus.rex@gmail.com>
2025-07-16 10:06:47 +0300

    add brain-even game

--- Makefile
+++ Makefile
@@ -26,6 +26,9 @@ help:
 brain-games:
 	node bin/brain-games.js
 
+brain-even:
+	node bin/brain-even.js
+
 publish:
 	npm publish --dry-run
 
--- src/is-even.js
+++ src/is-even.js
@@ -1,22 +1,42 @@
 import readlineSync from 'readline-sync';
+import greetUser from '../src/cli.js';
 
+const userName = greetUser();
 const isEven = (num) => num % 2 === 0;
 const getRandomInt = (min, max) =>
   Math.floor(Math.random() * (max - min + 1)) + min;
 
+console.log(`Answer "yes" if the number is even, otherwise answer "no".`);
+
 const game = () => {
+  let isCorrect = true;
+  let answer;
+  let correctAnswer;
   const min = 0;
   const max = 100;
-  const randomNum = getRandomInt(min, max);
-  const answer = readlineSync.question(
-    `Answer "yes" if the number is even, otherwise answer "no".\nQuestion: ${randomNum}\n`,
-  );
 
-  if (isEven(randomNum) && answer.toLowerCase() === 'yes') {
-    console.log('Correct!');
-  } else if (!isEven(randomNum) && answer.toLowerCase() === 'no') {
-    console.log('Correct!');
+  for (let i = 0; i < 3; i += 1) {
+    const randomNum = getRandomInt(min, max);
+    answer = readlineSync.question(`Question: ${randomNum}\nYour answer: `);
+
+    isEven(randomNum) ? (correctAnswer = 'yes') : (correctAnswer = 'no');
+
+    if (answer.toLowerCase() === correctAnswer) {
+      console.log(`Correct!`);
+    } else {
+      isCorrect = false;
+      break;
+    }
+  }
+
+  if (isCorrect) {
+    console.log(`Congratulations, ${userName}!`);
   } else {
-    console.log('Wrong answer. Try again!');
+    console.log(
+      `'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`,
+    );
+    console.log(`Let's try again, ${userName}!`);
   }
 };
+
+export default game;
