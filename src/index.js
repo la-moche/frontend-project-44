21c1e89d89dc26547d62e97e9b3b79cce4daf3a9
Alexander Smirnov <priamus.rex@gmail.com>
2025-07-20 15:35:03 +0300

    add src/index.js

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import readlineSync from 'readline-sync';
+import greetUser from './cli.js';
+import crypto from 'crypto';
+
+const MAX_ROUNDS = 3; // Максимальное количество раундов
+
+const getRandomInt = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  const randomBuffer = crypto.randomBytes(4);
+  const randomValue = randomBuffer.readUInt32BE(0);
+  return (randomValue % (max - min + 1)) + min;
+};
+
+const playGame = (gameLogic) => {
+  const userName = greetUser();
+  console.log(gameLogic.intro);
+
+  for (let i = 0; i < MAX_ROUNDS; i++) {
+    const { question, correctAnswer } = gameLogic.getQuestion();
+    const userAnswer = readlineSync.question(`Question: ${question}\nYour answer: `);
+
+    if (userAnswer.toLowerCase() === correctAnswer) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${userName}!`);
+      return;
+    }
+  }
+
+  console.log(`Congratulations, ${userName}!`);
+};
+
+export { getRandomInt, playGame };
